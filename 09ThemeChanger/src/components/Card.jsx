cde8f3bae86f95d2dc7e813d8387da11569e66d1
ankush9302 <105764023+ankush9302@users.noreply.github.com>
2024-09-07 16:38:56 +0530

    adding theme changer

---
+++ 09ThemeChanger/src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="/">
+                <img className="p-8 rounded-t-lg" src="/cardImage.png" alt="product_image1" />
+            </a>
+            <div className="px-5 pb-5">
+                <a href="/">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                        Welcome to Unveil
+                    </h5>
+                </a>
+                
+                <div className="flex items-center justify-between">
+                     <a
+                        href="/"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                       Enter as Guest
+                    </a>
+                    <a
+                        href="/"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Login/SignUp
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
